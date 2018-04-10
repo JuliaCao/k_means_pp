3c38735f3cc00eb80c213773457af19b6be8f943
ardunn <ardunn@lbl.gov>
2018-04-10 15:26:06 -0700

    Add functions for plotting and comparison in python

--- serial/serial.py
+++ serial/serial.py
@@ -1,16 +1,21 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
 
 # Hyper parameters
-K = 2
 M = 2
-N = 100
+K = 10
+N = 1000
 
 
-# def kmean_serial():
+def generate_data():
+    data = np.random.randn(N, M)
+    # data = np.random.rand(N, M)
+    return data
 
-def kpp_serial(X, k):
+def kpp_serial(X, K):
     # n = X.shape[0]
     # m = X.shape[1]
     D = np.zeros(N)
@@ -32,12 +37,6 @@ def kpp_serial(X, k):
         print("C[j] selected:", C[j])
     return C
 
-
-def generate_data():
-    data = np.random.rand(N, M)
-    return data
-
-
 def weighted_rand_index(W):
     r = np.sum(W) * np.random.rand(1)[0]
     i = 0
@@ -48,15 +47,42 @@ def weighted_rand_index(W):
     return i
 
 
-def plot_kmeans_pp(x, c):
-    if x.shape[1] != 2 or c.shape[1] != 2:
+def plot_kmeans_pp():
+    if X.shape[1] != 2 or C.shape[1] != 2:
         raise ValueError("M must be 2 dimensions to plot.")
-    plt.scatter(x[:, 0], x[:, 1], color='black')
-    plt.scatter(c[:, 0], c[:, 1], color='red')
+    plt.scatter(X[:, 0], X[:, 1], color='black')
+    plt.scatter(C[:, 0], C[:, 1], color='red')
     plt.show()
+
+
+def plot_kmeans():
+    if X.shape[1] != 2 or C.shape[1] != 2:
+        raise ValueError("M must be 2 dimensions to plot.")
+    km = KMeans(n_clusters=K, init=C, n_init=1)
+    labels = km.fit_predict(X)
+    rancols = [np.random.rand(3).tolist() for _ in range(max(labels) + 1)]
+    colmap = dict(zip(range(max(labels) + 1), rancols))
+    colors = [colmap[l] for l in labels]
+    for i in range(X.shape[0]):
+        plt.scatter(X[i][0], X[i][1], c=colors[i], s=3)
+    plt.show()
+
+def compare_kmeans(nruns=1000):
+    initnames = ["ours", "sklearn", "random"]
+    for i, init in enumerate([C, 'k-means++', 'random']):
+        iters = []
+        for run in range(nruns):
+            # print("Run {} in {}".format(run, init))
+            km = KMeans(n_clusters=K, init=init, n_init=1)
+            km.fit_predict(X)
+            iters.append(km.n_iter_)
+        print("Average {} n_iters needed: {}".format(initnames[i], np.mean(iters)))
 
 
 if __name__ == "__main__":
     X = generate_data()
     C = kpp_serial(X, K)
-    plot_kmeans_pp(X, C)
+
+    # compare_kmeans()
+    plot_kmeans_pp()
+    # plot_kmeans()
