ad7feb8d562e3707b7fb9b99e9560a21921acf68
Julia Cao <jcao2@br018.pvt.bridges.psc.edu>
2018-04-24 21:14:11 -0400

    debugging omp

--- omp/omp.cpp
+++ omp/omp.cpp
@@ -4,6 +4,9 @@
 #include <functional>
 #include <cstdint>
 #include <omp.h>
+#include <iostream>
+#include <ctime>
+#include <vector>
 
 #if defined __GNUC__ || defined __APPLE__
 #include <Eigen/Dense>
@@ -14,22 +17,10 @@
 using namespace std;
 using namespace Eigen;
 
-#define M 2
-#define K 10
-#define N 1000
+#define M 3
+#define K 5
+#define N 100
 
-// template<typename Rand>
-//  void generate_data(MatrixXd& data, Rand& r){
-// 	// MatrixXf data(N,M);
-
-// 	for(int i = 0;i<N;i++){
-// 		for(int j = 0; j < M ; j++){
-// 			data(i,j) = r();
-// 		}
-// 	}
-
-// 	return;
-// }
 
 template<typename Rand>
 int weighted_rand_index(VectorXd& W,Rand& r){
@@ -44,10 +35,8 @@ int weighted_rand_index(VectorXd& W,Rand& r){
 	return i;
 }
 
-
 template<typename Rand>
 void kpp_openmp(MatrixXd& X,MatrixXd& C, Rand& r){
-
 
     int p = omp_get_num_threads();//#threads
     vector<int> I(p,0);
@@ -76,7 +65,7 @@ void kpp_openmp(MatrixXd& X,MatrixXd& C, Rand& r){
 
 		    S(t) = 0.0f;
 
-		    for(auto i = lo;i<hi; i++){
+		    for(int i = lo;i<hi; i++){
 		    	if(j == 1){
 		    		D(i) = (C.row(j-1) - X.row(i)).norm();
 		    	}
@@ -100,58 +89,26 @@ void kpp_openmp(MatrixXd& X,MatrixXd& C, Rand& r){
       int i = I[sub_t];
       C(j) = X(i);
     }
-
     return;
-
 }
-
-// def output_kmeans_pp():
-//     if X.shape[1] != 2 or C.shape[1] != 2:
-//         raise ValueError("M must be 2 dimensions to plot.")
-//     plt.scatter(X[:, 0], X[:, 1], color='black')
-//     plt.scatter(C[:, 0], C[:, 1], color='red')
-//     plt.show()
-
-
-// def plot_kmeans():
-//     if X.shape[1] != 2 or C.shape[1] != 2:
-//         raise ValueError("M must be 2 dimensions to plot.")
-//     km = KMeans(n_clusters=K, init=C, n_init=1)
-//     labels = km.fit_predict(X)
-//     rancols = [np.random.rand(3).tolist() for _ in range(max(labels) + 1)]
-//     colmap = dict(zip(range(max(labels) + 1), rancols))
-//     colors = [colmap[l] for l in labels]
-//     for i in range(X.shape[0]):
-//         plt.scatter(X[i][0], X[i][1], c=colors[i], s=3)
-//     plt.show()
-
-// def compare_kmeans(nruns=1000):
-//     initnames = ["ours", "sklearn", "random"]
-//     for i, init in enumerate([C, 'k-means++', 'random']):
-//         iters = []
-//         for run in range(nruns):
-//             # print("Run {} in {}".format(run, init))
-//             km = KMeans(n_clusters=K, init=init, n_init=1)
-//             km.fit_predict(X)
-//             iters.append(km.n_iter_)
-//         print("Average {} n_iters needed: {}".format(initnames[i], np.mean(iters)))
-
 
 
 int main( int argc, char** argv ){
+	srand((unsigned int)time(0));	
 
 	random_device rd;
 	// std::mt19937 e2(rd());
 	uniform_real_distribution<double> dist(-1.f, 1.f);
 	uniform_real_distribution<double> zero_one(0.f, 1.f);
-	auto mat_rand = bind(dist,ref(rd));
+	//auto mat_rand = bind(dist,ref(rd));
 	auto weight_rand = bind(zero_one,ref(rd));
 
 	MatrixXd X = MatrixXd::Random(N,M);
 	MatrixXd C(K,M);
 
+	cout << "X" << X << endl;	
 
 	// generate_data(X,mat_rand);
-  kpp_openmp(X, C, weight_rand);
+  	kpp_openmp(X, C, weight_rand);
 	// output_kmeans_pp()
 }
