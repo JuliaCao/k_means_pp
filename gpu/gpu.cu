417027be7cc52b687185ddab36539dd8f2fb2537
ardunn <ardunn@lbl.gov>
2018-04-24 23:48:05 -0700

    autocommit

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -203,11 +203,11 @@ int main( int argc, char** argv ){
 
 
 	// Initializing Data
-	cout << "RUNNING KMEANS++ SERIAL WITH SAME " << n << " POINTS , " << k << " CLUSTERS, AND " << m << " DIMENSIONS.\n";
-	// Running serial simulation
-	double t2 = read_timer( );
-  kpp_serial(n, k, X_serial, C_serial, weight_rand_serial);
-	double t3 = read_timer( ) - t2;
-	cout << "THE SERIAL/CPU SIMULATION TOOK " << t3 << " SECONDS. \n";
-	cout << "THE RESULTING SPEEDUP IS: " << t3/t1 << sep;
+	// cout << "RUNNING KMEANS++ SERIAL WITH SAME " << n << " POINTS , " << k << " CLUSTERS, AND " << m << " DIMENSIONS.\n";
+	// // Running serial simulation
+	// double t2 = read_timer( );
+  // kpp_serial(n, k, X_serial, C_serial, weight_rand_serial);
+	// double t3 = read_timer( ) - t2;
+	// cout << "THE SERIAL/CPU SIMULATION TOOK " << t3 << " SECONDS. \n";
+	// cout << "THE RESULTING SPEEDUP IS: " << t3/t1 << sep;
 }
