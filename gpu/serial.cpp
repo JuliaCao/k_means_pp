8b17326d147a36b5142c3ff6aaa222ae52f31ecb
ardunn <ardunn@lbl.gov>
2018-04-24 17:31:56 -0700

    autocommit

--- gpu/serial.cpp
+++ gpu/serial.cpp
@@ -58,39 +58,6 @@ void kpp_serial(MatrixXd& X, MatrixXd& C, Rand& r) {
 	return;
 }
 
-// def output_kmeans_pp():
-//     if X.shape[1] != 2 or C.shape[1] != 2:
-//         raise ValueError("M must be 2 dimensions to plot.")
-//     plt.scatter(X[:, 0], X[:, 1], color='black')
-//     plt.scatter(C[:, 0], C[:, 1], color='red')
-//     plt.show()
-
-
-// def plot_kmeans():
-//     if X.shape[1] != 2 or C.shape[1] != 2:
-//         raise ValueError("M must be 2 dimensions to plot.")
-//     km = KMeans(n_clusters=K, init=C, n_init=1)
-//     labels = km.fit_predict(X)
-//     rancols = [np.random.rand(3).tolist() for _ in range(max(labels) + 1)]
-//     colmap = dict(zip(range(max(labels) + 1), rancols))
-//     colors = [colmap[l] for l in labels]
-//     for i in range(X.shape[0]):
-//         plt.scatter(X[i][0], X[i][1], c=colors[i], s=3)
-//     plt.show()
-
-// def compare_kmeans(nruns=1000):
-//     initnames = ["ours", "sklearn", "random"]
-//     for i, init in enumerate([C, 'k-means++', 'random']):
-//         iters = []
-//         for run in range(nruns):
-//             # print("Run {} in {}".format(run, init))
-//             km = KMeans(n_clusters=K, init=init, n_init=1)
-//             km.fit_predict(X)
-//             iters.append(km.n_iter_)
-//         print("Average {} n_iters needed: {}".format(initnames[i], np.mean(iters)))
-
-
-
 int main( int argc, char** argv ){
 
 	for (int i = 1; i < argc; ++i) {
