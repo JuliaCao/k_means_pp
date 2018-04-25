bf17ddec2141405fa7b956430cc781c513f7c659
ardunn <ardunn@lbl.gov>
2018-04-24 18:30:10 -0700

    autocommit

--- gpu/gpu.cu
+++ gpu/gpu.cu
@@ -11,6 +11,12 @@
 #include <thrust/transform.h>
 #include <thrust/reduce.h>
 
+/*
+Author: Alexander Dunn
+University of California, 2018
+CS 267 Final Project: Parallelizing K-means++ Initialization
+*/
+
 #if defined __GNUC__ || defined __APPLE__
 #include <Eigen/Dense>
 #else
@@ -94,33 +100,66 @@ void kpp_gpu(int n, int k, MatrixXd &X, MatrixXd &C, Rand &r) {
 	return;
 }
 
+template<typename Rand>
+void kpp_serial(int n, int k, MatrixXd &X, MatrixXd &C, Rand &r) {
+
+	VectorXd D(n);
+	for(int i  = 0 ; i < n ; i++){
+		D(i) = numeric_limits<float>::max();
+	}
+
+	// The first seed is selected uniformly at random
+	int index = (int)(r() * n);
+	C.row(0) = X.row(index);
+	for(int j = 1; j < k; j++){
+			for(auto i = 0; i < n;i++){
+					VectorXd c = C.row(j-1);
+					VectorXd x = X.row(i);
+					VectorXd tmp = c - x;
+				D(i) = min(tmp.norm(), D(i));
+			}
+
+		int i = weighted_rand_index(D,r);
+	C.row(j) = X.row(i);
+	}
+	return;
+}
+
 
 int main( int argc, char** argv ){
 
-	std::string sep = "\n----------------------------------------\n";
+	// Parsing commands and prettify-ing output
+	std::string sep = "\n-----------------------------------------------------------------------------------------------------------\n";
 	int n = read_int( argc, argv, "-n", 1000);
 	int m = read_int( argc, argv, "-m", 2);
 	int k = read_int( argc, argv, "-k", 10);
 
+	// Initializing Data
 	cout << sep << "RUNNING KMEANS++ GPU WITH " << n << " POINTS , " << k << " CLUSTERS, AND " << m << " DIMENSIONS.\n";
-
 	random_device rd;
 	// std::mt19937 e2(rd());
-	uniform_real_distribution<double> kmdata(-1.f, 1.f);
 	uniform_real_distribution<double> zero_one(0.f, 1.f);
 	auto weight_rand = bind(zero_one, ref(rd));
-
 	MatrixXd X = MatrixXd::Random(n, m);
-	//cout << X << sep;
 	MatrixXd C(k, m);
-
-	// auto mat_rand = bind(kmdata, ref(rd));
-	// generate_data(X,mat_rand);
+	// Running GPU simulation
 	double t0 = read_timer( );
   kpp_gpu(n, k, X, C, weight_rand);
 	double t1 = read_timer( ) - t0;
-
 	cout << "THE GPU SIMULATION TOOK " << t1 << " SECONDS." << sep;
-	//cout << C << sep;
-	// output_kmeans_pp()
+
+
+	// Initializing Data
+	cout << sep << "RUNNING KMEANS++ SERIAL WITH " << n << " POINTS , " << k << " CLUSTERS, AND " << m << " DIMENSIONS.\n";
+	random_device rd;
+	// std::mt19937 e2(rd());
+	uniform_real_distribution<double> zero_one(0.f, 1.f);
+	auto weight_rand = bind(zero_one, ref(rd));
+	MatrixXd X = MatrixXd::Random(n, m);
+	MatrixXd C(k, m);
+	// Running serial simulation
+	double t0 = read_timer( );
+  kpp_serial(n, k, X, C, weight_rand);
+	double t1 = read_timer( ) - t0;
+	cout << "THE SERIAL/CPU SIMULATION TOOK " << t1 << " SECONDS." << sep;
 }
