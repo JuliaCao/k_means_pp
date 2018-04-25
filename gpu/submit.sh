559b8ff33e3a1260b9afcb13119292ee4294f8e3
ardunn <ardunn@lbl.gov>
2018-04-24 18:33:11 -0700

    autocommit

--- gpu/submit.sh
+++ gpu/submit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #SBATCH -A cc3uv3p # 2017 XSEDE Applications of Parallel Computing Course Allocation
-#SBATCH -J auto-particle-gpu
-#SBATCH -o auto-particle-gpu.stdout
+#SBATCH -J kmeans++
+#SBATCH -o results.txt
 #SBATCH -p GPU-shared
 #SBATCH --gres=gpu:k80:1
 #SBATCH -t 00:20:00
 #SBATCH -N 1
+
+
+./gpu -n 100000 -m 5 -k 20
